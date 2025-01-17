38d0c113fe2e9df6bdcdb00b6f4a683a0db50bf4
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-17 19:35:11 +0100

    feat: :white_check_mark: concerts relationship in Band model done

--- app/Models/Band.php
+++ app/Models/Band.php
@@ -9,4 +9,11 @@ class Band extends Model
 {
     /** @use HasFactory<\Database\Factories\BandFactory> */
     use HasFactory;
+
+    public function concerts()
+    {
+        return $this->belongsToMany(Concert::class, 'concert_band', 'band_id', 'concert_id')
+                    ->as('ConcertBand')
+                    ->withTimestamps();
+    }
 }
