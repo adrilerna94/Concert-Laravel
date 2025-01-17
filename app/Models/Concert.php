d3c732d28cd60cd1f5030f78cb098fe3f7bb9ee5
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-17 19:32:19 +0100

    feat: :art: bands relationship in Concert model done

--- app/Models/Concert.php
+++ app/Models/Concert.php
@@ -9,4 +9,12 @@ class Concert extends Model
 {
     /** @use HasFactory<\Database\Factories\ConcertFactory> */
     use HasFactory;
+
+    public function bands()
+    {
+        return $this->belongsToMany(Band::class, 'concert_band', 'concert_id', 'band_id')
+                    ->as('ConcertBand') // en vez de pivot se llamará ConcertBand
+                    ->withTimestamps(); // created at updated at (timestamps columnas) aparecerán
+                    // default: solo las FK Y PK aparecen en la tabla intermedia (pivot por default)
+    }
 }
